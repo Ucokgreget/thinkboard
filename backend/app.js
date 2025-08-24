7469452ff07a297a872b8b99a7ae8ceea1e65786
Ucokgreget <luqmanalfauzan1738@gmail.com>
2025-08-24 16:44:37 +0700

    prepare deployment

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,46 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
 const connectDB = require('./config/connect');
-const noteRouter = require('./route/noteRoute')
+const noteRouter = require('./route/noteRoute');
 const notFound = require('./middleware/notFound');
 const errorHandlerMiddleware = require('./middleware/errorHandler');
 const rateLimiter = require('./middleware/rateLimiter');
+const path = require('path');
 
-app.use(cors())
-app.use(express.json())
-app.use(rateLimiter)
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors());
+}
+app.use(express.json());
+app.use(rateLimiter);
 
+// API routes
+app.use('/api/v1/note', noteRouter);
 
-app.use('/api/v1/note', noteRouter)
+const dirname = path.resolve();
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(dirname, "../frontend/dist")));
 
-app.use(notFound)
-app.use(errorHandlerMiddleware)
-
-
-const port = 3000;
-const start = async () => {
-    try{
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, () => console.log(`server is listening on ${port}`))
-    }
-    catch(err){
-        console.log(err)
-    }
-    
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(dirname, "../frontend/dist", "index.html"));
+  });
 }
 
-start()
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+
+
+const port = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`Server is listening on ${port}`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
